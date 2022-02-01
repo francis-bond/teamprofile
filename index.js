e999003e8c56e7aa9adbdaad0b7f2155a2af3b8e
Francis Bond <francisbond1996@gmail.com>
2022-01-31 22:44:11 -0600

    Added pseudocode to show how the application should work.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Entern = require('./lib/Intern');
 const Manager = require('./lib/Manager')
-const render = require('./src/page-template')
+const render = require('./src/page-template');
+const generatePage = require('./src/page-template');
 const teamMembers = [];
 const idArray = [];
 
@@ -27,12 +28,6 @@ const managerQuestions = [
         type: 'input',
         message: "Enter office number",
         name: 'officeNumber'
-    },
-    {
-        type: 'list',
-        message: "Add engineer/inter or finish building team",
-        name: 'nextstep',
-        choices: ["Intern", "Engineer", "Im done building my team"]
     },
 ];
 
@@ -93,3 +88,40 @@ const internQuestions = [
         choices: ["Intern", "Engineer", "Im done building my team"]
     },
 ]
+
+function start() {
+    inquirer
+        .prompt(managerQuestions)
+        .then((response) => {
+            
+        })
+}
+
+function start() {
+    inquirer
+        .prompt(managerQuestions)
+        .then((response) => {
+            // function that makes new class from
+            if (response.nextstep === "Intern"){
+                makeIntern();
+            } else if(response.nextstep === "Engineer"){
+                makeEngineer();
+            } else{
+
+            }
+        })
+}
+
+start();
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, generatePage(data)), (err) =>
+        err ? console.log(err) : console.log('team.html created')
+}
+
+// Use inquirer to get inputs from user
+// Push data for each team member into and array to be iterated through
+// filter by employee type and send arrays to class constructors to add classes
+// us the page-template.js to add cards to get html page script with the cards created for each employee
+// create a team.html file and write to to it the info we get from page template
+// create tests to test the function in the class constructors
